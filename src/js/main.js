5d9abca9fac106ac5201b8ecdb90247db3b87e72
fasfsafas <titovalexandr03092010@gmail.com>
2026-04-11 17:16:37 +0500

    скрипт на модальное окно

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+function openModal(isOpen){
+    const modal = document.querySelector('.modal-container')
+    if(isOpen){
+        modal.style.display = 'block'
+        modal.style.height = '100vh'
+    }
+    else{
+        modal.style.display = 'none'
+    }
+}
+
+// Закрытие модального окна по клику на кнопку "back"
+document.addEventListener('DOMContentLoaded', function(){
+    const backButton = document.querySelector('.modal-header .back')
+    if(backButton){
+        backButton.addEventListener('click', function(){
+            openModal(false)
+        })
+    }
+    
+    // Закрытие модального окна по клику на фон
+    const modalContainer = document.querySelector('.modal-container')
+    if(modalContainer){
+        modalContainer.addEventListener('click', function(e){
+            if(e.target === this){
+                openModal(false)
+            }
+        })
+    }
+})
